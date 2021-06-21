84a4487eea91296dbe41ea310f9a001296fc94d2
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2021-06-21 16:31:55 +0100

    created project file structure and html structure completed, Working on mobile view

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+const hamburgerMenu = document.querySelector('.nav--hamburger');
+const closeNavBtn = document.querySelector('.nav--close');
+const navList = document.querySelector('.nav__list');
+
+
+
+
+
+// functions
+function showNavList() {
+    hamburgerMenu.style.display = 'none';
+    navList.style.display = 'flex';
+    closeNavBtn.style.display = 'block';    
+}
+
+function closeNavList() {
+    hamburgerMenu.style.display = 'block';
+    navList.style.display = 'none';
+    closeNavBtn.style.display = 'none';
+}
+
+
+//event listeners
+hamburgerMenu.addEventListener('click', showNavList);
+closeNavBtn.addEventListener('click', closeNavList);
